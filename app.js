ef49f237355827f982bfd047dcfc71a44e9315c2
hudaman0621 <mrhu828@gmail.com>
2020-05-17 23:52:50 -0400

    remake 3rd time

--- app.js
+++ app.js
@@ -1,138 +1,92 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-app.use(bodyParser.json()); //also needs json to handle json
-app.use(bodyParser.urlencoded({ extended: false })); //handles the bodyParser
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'));
-
-const todoList = [
+let todoList = [ // if you assigned the todoList to a const, it cannot be deleted. .....
   {
     id: 1,
-    todo: 'Implement a REST API',
+    todo: 'remake 3',
   },
-
   {
     id: 2,
-    todo: '2Implement a REST API',
+    todo: 'try 2',
   },
-
   {
     id: 3,
-    todo: '3Implement a REST API',
+    todo: '3???',
   },
-
   {
     id: 4,
-    todo: '4Implement a REST API',
+    todo: '4Profit!',
   },
 ];
-
 // GET /api/todos
 app.get('/api/todos', (req, res) => {
-  res.json(todoList);
+  res.json(todoList)
 })
-
 // GET /api/todos/:id
-app.get ('/api/todos/:id', (req, res) => {
-  const todo = 
-  todoList.find((todo) => {
-    return todo.id === Number.parseInt(req.params.id);
-  }) || {};
+app.get('/api/todos/:id', (req, res) => {
+  const todo =
+    todoList.find((todo) => {
+      return todo.id === Number.parseInt(req.params.id);
+    }) || {};
   const status = Object.keys(todo).length ? 200 : 404;
   res.status(status).json(todo);
-})
+});
 // POST /api/todos
 app.post('/api/todos', (req, res) => {
-  if (!req.body.todo) {
-    res.status(400).json({
-      error: 'Please provide todo text',  //provide an error 
-    });
-  } else {
-  //get all ids
-  //find max
-  //add 1 
+  if(req.body.todo) {
   const maxId = todoList.reduce((max, currentTodo) => {
-    if (currentTodo.id > max) {  //comparing the array to see if the next ID is greater then the previous ID 
-      max = currentTodo.id;       //if it is, it will move on to the next one, if not, it will set the id to the NEW id and return the max
+    if (currentTodo.id > max) {
+      max = currentTodo.id;
     }
-    return max; 
-  }, 0)
-
-  // res.json(req.body);
-  const newTodo = {  //create the new todo 
+    return max;
+  }, 0);
+  const newTodo = {
     id: maxId + 1,
     todo: req.body.todo,
-  }
-  todoList.push(newTodo); //push the new to do into the array
-  res.json(newTodo); //respond with the thing we just created
+  };
+  todoList.push(newTodo);
+  res.json(newTodo);
+  } else {
+    res.status(400).json({
+      error: "fix it"
+    });
   };
 });
-
 // PUT /api/todos/:id
 app.put('/api/todos/:id', (req, res) => {
-//   // const todo = 
-//   // todoList.find((todo) => {
-//   //   return todo.id === Number.parseInt(req.params.id);
-//   // }) || {};
-//   // const status = Object.keys(todo).length ? 200 : 404;
-//   // res.status(status).json(todo);
-//   const found = todoList.find((todo) => {
-//     if (found) {
-//       let updateTodo = req.body;
-//       found.forEach(todoList => {
-//         if (todoList.id === parseInt(req.params.id)){
-//           todoList.id = updateTodo.id ? updateTodo.id : todoList.id;
-//           todoList.todo = updateTodo.todo ? updateTodo.todo : todoList.todo;
-          
-//           res.json({msg: "Id updated:", id})
-//         }
-//         return todo.id === Number.parseInt(req.params.id);
-//     });
-//   } else {
-//     res.status(400).json({ msg: `no id found ${req.params.id}` });
-//   }
-//   })
-// });
-
-  if (!req.body || !req.body.todo) {
+  if(!req.body || !req.body.todo) {
     res.status(400).json({
-      error: 'Please provide todo text',  //provide an error 
+      error: "does not work"
     });
-    return;
+    return; 
   }
-  let updateTodo = {};
-  todoList.forEach((todo) =>{
-    if (todo.id === Number.parseInt(req.params.id)) {
-      todo.todo =req.body.todo;
-      updateTodo = todo;
+  let updatedToDoPair = {};
+  todoList.forEach((todoPair) => {
+    if (todoPair.id === Number.parseInt(req.params.id)) {
+      todoPair.todo = req.body.todo;
+      updatedToDoPair = todoPair;
     }
-  })
-  const status = Object.keys(updateTodo).length ? 200 : 404;
-  res.status(status).json(updateTodo);
+  });
+  const status = Object.keys(updatedToDoPair).length ? 200 : 404;
+  res.status(status).json(updatedToDoPair);
 });
 // DELETE /api/todos/:id
-
-app.listen(3001, function () {
-  console.log('Todo List API is now listening on port 3001...');
+app.delete('/api/todos/:id', (req, res) => {
+  let toBeDel = false;
+  todoList = todoList.filter((todo) => {
+    if (todo.id === Number.parseInt(req.params.id)) {
+      toBeDel = true;
+      return false;
+    }
+    return true;
+  })
+  const status = toBeDel ? 200 : 404;
+  res.status(status).json(todoList);
+}); 
+app.listen(3000, function () {
+  console.log('Todo List API is now listening on port 3000...');
 });
-
-
-
-// if (!req.body || !req.body.todo) {
-//   res.status(400).json({
-//     error: 'Provide todo text',
-//   });
-//   return;
-// }
-// let updatedTodo = {};
-// todoList.forEach((todo) => {
-//   if (todo.id === Number.parseInt(req.params.id)) {
-//     todo.todo = req.body.todo;
-//     updatedTodo = todo;
-//   }
-// });
-// const status = Object.keys(updatedTodo).length ? 200 : 404;
-// res.status(status).json(updatedTodo);
-// });
